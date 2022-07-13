fc2094d701c3d7c00458fa8d9ababc3b59d1117f
bibitto <bibitto.crypto@gmail.com>
2022-07-13 20:11:05 +0900

    fix env

--- pages/api/mail.ts
+++ pages/api/mail.ts
@@ -1,18 +1,24 @@
 import { createTransport } from 'nodemailer';
+require('dotenv').config();
 
-const postMail = async (req, res) => {
+const postMail = async (
+    req: { body: any },
+    res: {
+        status: (arg0: number) => { (): any; new (): any; json: { (arg0: { success: boolean }): void; new (): any } };
+    }
+) => {
     const transporter = createTransport({
         service: 'gmail',
         port: 465,
         secure: true,
         auth: {
-            user: 'bibitto.crypto@gmail.com', //process.env.MAIL_USER,
-            pass: 'nbkritryaaafderr', //process.env.MAIL_PASS,
+            user: process.env.MAIL_FROM,
+            pass: process.env.GMAIL_PASS,
         },
     });
     await transporter.sendMail({
-        from: 'bibitto.crypto@gmail.com', //process.env.MAIL_USER,
-        to: 'kawano.sho1118@gmail.com', //process.env.MAIL_TO,
+        from: process.env.MAIL_FROM,
+        to: process.env.MAIL_TO,
         subject: 'お問い合わせ from ブログ',
         text: req.body,
     });
